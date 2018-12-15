a22ab7aaff44f78c0ef88805c98e8b51df640476
hirafoo <hirafoo@cpan.org>
2018-12-15 20:47:45 +0900

    impl C

---
+++ web/src/main/java/net/hirafoo/springboot2demo/springboot2demo/controller/RootController.java
@@ -0,0 +1,12 @@
+package net.hirafoo.springboot2demo.springboot2demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
