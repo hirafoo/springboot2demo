9392734587ae6be98efa07ce55698198ee40ff7b
hirafoo <hirafoo@cpan.org>
2018-12-19 02:25:13 +0900

    rename

--- web/src/main/java/net/hirafoo/springboot2todoapp/Springboot2TodoAppApplication.java
+++ web/src/main/java/net/hirafoo/springboot2todoapp/Springboot2TodoAppApplication.java
@@ -1,13 +1,13 @@
-package net.hirafoo.springboot2demo.springboot2demo;
+package net.hirafoo.springboot2todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot2demoApplication {
+public class Springboot2TodoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot2demoApplication.class, args);
+		SpringApplication.run(Springboot2TodoAppApplication.class, args);
 	}
 
 }
