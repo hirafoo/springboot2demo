54bab7b9e44fff121f0d2c2bef930841955f971e
hirafoo <hirafoo@cpan.org>
2018-12-20 01:47:36 +0900

    say hello

--- web/src/main/java/net/hirafoo/todo/controller/RootController.java
+++ web/src/main/java/net/hirafoo/todo/controller/RootController.java
@@ -1,12 +1,25 @@
 package net.hirafoo.todo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import net.hirafoo.todo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+//@Controller
 public class RootController {
+    @Autowired
+    HelloService helloService;
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+        //log.info("{}", "hoge");
+        String hello = helloService.hello("hoge");
+        //model.addAttribute("hello", hello);
+        return hello;
     }
 }
