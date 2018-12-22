7812da68ff940b6d537173c89570ac54de257757
hirafoo <hirafoo@gmail.com>
2018-12-22 21:57:58 +0900

    add model

--- web/src/main/java/net/hirafoo/todo/controller/ApiController.java
+++ web/src/main/java/net/hirafoo/todo/controller/ApiController.java
@@ -1,7 +1,7 @@
 package net.hirafoo.todo.controller;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import net.hirafoo.todo.model.Item;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,23 +14,11 @@ import java.util.List;
 public class ApiController {
 
     @GetMapping("/api/list")
-    public List<Item> list(Pageable pageable) {
-        log.info("{}", pageable);
+    public List<Item> list() {
         List<Item> items = new ArrayList<Item>();
         for (int i = 0; i < 20; i++) {
             items.add(new Item(i, "name_" + i));
         }
         return items;
-    }
-
-    @Data
-    public class Item {
-        private int id;
-        private String name;
-        public Item(int id, String name) {
-            super();
-            this.id = id;
-            this.name = name;
-        }
     }
 }
