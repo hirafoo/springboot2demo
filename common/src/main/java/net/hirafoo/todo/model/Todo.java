fb978a761392c8ad524e6fae6db0ab2e30efbee3
hirafoo <hirafoo@cpan.org>
2019-01-05 14:44:19 +0900

    suddenly mybatis

--- common/src/main/java/net/hirafoo/todo/model/Todo.java
+++ common/src/main/java/net/hirafoo/todo/model/Todo.java
@@ -4,14 +4,10 @@ import lombok.Data;
 
 @Data
 public class Todo {
-    Long id;
-    String name;
-    String description;
-
-    public Todo(Long id, String name, String description) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+    public Long id;
+    public String name;
+    public String description;
+    // TODO why long?
+    public Long createdAt;
+    public Long updatedAt;
 }
