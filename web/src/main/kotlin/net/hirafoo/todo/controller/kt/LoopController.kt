3dad1c4b52215fcdbac2abffff2c7078a2b506bc
hirafoo <hirafoo@gmail.com>
2020-12-24 17:40:44 +0900

    i

--- web/src/main/kotlin/net/hirafoo/todo/controller/kt/LoopController.kt
+++ web/src/main/kotlin/net/hirafoo/todo/controller/kt/LoopController.kt
@@ -1,5 +1,6 @@
 package net.hirafoo.todo.controller.kt
 
+import lombok.extern.slf4j.Slf4j
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -24,6 +25,21 @@ class LoopController {
                     result++
                     loop--
                 }
+            }
+        }
+        model["result"] = result
+        return "kt/loop/while_sample"
+    }
+
+    @GetMapping("for_sample")
+    fun forSample(
+        model: Model,
+        @RequestParam("num", required = false) num: Int?
+    ): String {
+        var result = 0
+        if (num != null) {
+            for (i in 0 until num) {
+                result += i
             }
         }
         model["result"] = result
