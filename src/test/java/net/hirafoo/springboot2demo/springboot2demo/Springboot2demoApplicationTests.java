1634189e473d377bf9c3115e6f15c2977efe0bed
hirafoo <hirafoo@gmail.com>
2020-12-24 21:30:21 +0900

    Revert "i"
    This reverts commit 1cd2e44b6bf1bde01ccb6a2b62522f6cc56ad927.

---
+++ src/test/java/net/hirafoo/springboot2demo/springboot2demo/Springboot2demoApplicationTests.java
@@ -0,0 +1,17 @@
+package net.hirafoo.springboot2demo.springboot2demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2demoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
