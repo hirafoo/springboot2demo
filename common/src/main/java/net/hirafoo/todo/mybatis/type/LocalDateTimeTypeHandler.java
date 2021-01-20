da7d00c28640a51dbeb1bc999a077ee4d2866b57
hirafoo <hirafoo@gmail.com>
2021-01-20 21:48:25 +0900

    fix getter

--- common/src/main/java/net/hirafoo/todo/mybatis/type/LocalDateTimeTypeHandler.java
+++ common/src/main/java/net/hirafoo/todo/mybatis/type/LocalDateTimeTypeHandler.java
@@ -5,10 +5,13 @@ import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedTypes;
 
 import java.sql.*;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @MappedTypes(LocalDateTime.class)
 public class LocalDateTimeTypeHandler extends BaseTypeHandler<LocalDateTime> {
+    public static final ZoneId ZONE_ID_DEFAULT = ZoneId.systemDefault();
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, LocalDateTime parameter, JdbcType jdbcType) throws SQLException {
@@ -17,32 +20,26 @@ public class LocalDateTimeTypeHandler extends BaseTypeHandler<LocalDateTime> {
 
     @Override
     public LocalDateTime getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        Timestamp timestamp = rs.getTimestamp(columnName);
-        if (timestamp == null) {
-            return null;
-        } else {
-            return timestamp.toLocalDateTime();
-        }
+        int epoch = rs.getInt(columnName);
+        LocalDateTime date =
+                LocalDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZONE_ID_DEFAULT);
+        return date;
     }
 
     @Override
     public LocalDateTime getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        Timestamp timestamp = rs.getTimestamp(columnIndex);
-        if (timestamp == null) {
-            return null;
-        } else {
-            return timestamp.toLocalDateTime();
-        }
+        int epoch = rs.getInt(columnIndex);
+        LocalDateTime date =
+                LocalDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZONE_ID_DEFAULT);
+        return date;
     }
 
     @Override
     public LocalDateTime getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        Timestamp timestamp = cs.getTimestamp(columnIndex);
-        if (timestamp == null) {
-            return null;
-        } else {
-            return timestamp.toLocalDateTime();
-        }
+        int epoch = cs.getInt(columnIndex);
+        LocalDateTime date =
+                LocalDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZONE_ID_DEFAULT);
+        return date;
     }
 }
 
